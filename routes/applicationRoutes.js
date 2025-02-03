ee3559e4660416aa6e06ead6d450ee47d74b1a2f
rkverma2001 <riteshkumarverma2018@gmail.com>
2025-02-03 15:26:03 +0530

    updated application routes

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { create, view, update, remove } = require("../controllers/applicationController");
+const authenticate = require("../middlewares/apiMiddleware");
 
 // Define application routes
 router.post("/create", create);
-router.get("/view", view);
+router.post("/view", authenticate, view);
 router.put("/update/:urn", update);
 router.delete("/remove/:urn", remove);
 
